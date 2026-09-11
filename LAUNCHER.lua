3fa1ef2efd604a3b6e9c17929450470ac8b44f86
blegbot1 <cithak46@gmail.com>
2026-09-11 20:19:39 +0300

    chore: update launcher cache buster

--- LAUNCHER.lua
+++ LAUNCHER.lua
@@ -5,7 +5,7 @@ local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local player = Players.LocalPlayer
 
-local HUB_URL = "https://raw.githubusercontent.com/blegbot1/ELITE-HUB-14.0-Universal/main/ELITE_HUB_14.0.lua?v=a26fb4b"
+local HUB_URL = "https://raw.githubusercontent.com/blegbot1/ELITE-HUB-14.0-Universal/main/ELITE_HUB_14.0.lua?v=6d2938b"
 local IMAGE_URL = "https://raw.githubusercontent.com/blegbot1/ELITE-HUB-14.0-Universal/main/launcher/kotik.jpg"
 local IMAGE_FILE = "elitehub_kotik.jpg"
 
